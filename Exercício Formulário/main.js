108829ba22fb29d22e175e79de2a84a4272670c1
Katiucia Tanus <125466617+sincejuly90@users.noreply.github.com>
2024-07-15 09:56:11 -0300

    Add files via upload

---
+++ Exercício Formulário/main.js
@@ -0,0 +1,31 @@
+const form = document.getElementById('form-confirm');
+
+let formValid = false;
+
+function numbervalid (ValA, ValB){
+    return ValB > ValA;
+}
+
+form.addEventListener('submit', function(e){
+    e.preventDefault(); //previne carregamento da página após "submit"
+    let ValA = parseFloat(document.getElementById('valor-a').value);
+    let ValB = parseFloat(document.getElementById('valor-b').value);
+
+    const messagesucess = '<b> ValorA menor que ValorB!</B>';
+    const containermessagesucess = document.querySelector('.success-message');
+    
+    formValid = numbervalid(ValA, ValB);
+    
+    if (formValid){
+        document.querySelector('.error-message').style.display = 'none';
+        containermessagesucess.innerHTML = messagesucess;
+        containermessagesucess.style.display = 'block';
+        document.querySelector('.error-message').style.display = 'none';
+
+        ValA.value = '';
+        ValB.value = '';
+    } else {
+        document.querySelector('.error-message').style.display = 'block';
+        containermessagesucess.style.display = 'none';
+    }    
+})
